03868982da2723019acc6c8614fa3a7675ece49d
shalini <shalinimano2595@gmail.com>
2026-05-05 16:50:37 +0200

    Updated project changes

--- src/main/java/com/newsletter/config/SecurityConfig.java
+++ src/main/java/com/newsletter/config/SecurityConfig.java
@@ -22,6 +22,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private static final String[] PUBLIC_URLS = {
+            "/",
+            "/error",
             "/api/auth/**",
             "/swagger-ui/**",
             "/swagger-ui.html",
